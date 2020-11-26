86adc2440c2ad2d639720d58b0e896cc7adaed18
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2020-11-26 11:46:07 -0300

    Se realizaron cambios

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
+// Require's
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Express
 var app = express();
 
-// view engine setup
+// Template Engine
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,20 +28,21 @@ app.get('/login', function (req, res){
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Set locals, only providing error in development  
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+// Render the error page  
+res.status(err.status || 500);
+res.render('error');
 });
 
 module.exports = app;
