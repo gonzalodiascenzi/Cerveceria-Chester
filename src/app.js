d36d001f67fa3c6262da78c98020fdccdf1456a2
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2021-01-21 12:14:12 -0300

    Solucionado: Se puede mostrar la vista del archivo html mediante las correctas aplicaciones de las funciones de express

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@
 var express = require('express');
 //Inicializo express en la variable app
 var app = express();
+//Defino la carpeta con los archivos estaticos (imagenes)
+app.use(express.static('public'));
 //Utilizo la funcion listen de express para inicializar el servidor
 app.listen(3000, () => {
   console.log('Servidor levantado exitosamente');
 })
 //Rutas y vistas
 app.get('/', (req, res) => {
-  res.render('views/index'); //sendFile, renderFile, render, send
+  res.sendFile(__dirname + '/views/index.html'); //sendFile, renderFile, render, send
 });
 
